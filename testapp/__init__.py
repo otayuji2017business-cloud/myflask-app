8c0a9e320150155bd6d941940aaffe7e6e56e7fb
ota-yuji <otayuji2017business@gmail.com>
2026-02-06 16:50:38 +0900

    switch to Cloud SQL

--- testapp/__init__.py
+++ testapp/__init__.py
@@ -1,5 +1,5 @@
 
-
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,19 +9,25 @@ app = Flask(__name__)
 # =====================
 # アプリ設定
 # =====================
-
-
 # app.config.from_object('testapp.config') # 今は無効化しておく
-
 
 # SECRET_KEY（Flask必須。簡易でOK）
 app.config['SECRET_KEY'] = 'dev-secret-key'
 
+
 # SQLite（学習用）→cloud SQLを入れます。
-# Cloud Run では永続化されないが、今はOK
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    "mysql+pymysql://flaskuser:PASSWORD@/myflaskdb"
-    "?unix_socket=/cloudsql/PROJECT_ID:asia-northeast1:INSTANCE_NAME"
+# =====================
+# Cloud SQL 設定
+# =====================
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+DB_NAME = os.environ.get("DB_NAME")
+INSTANCE_CONNECTION_NAME = os.environ.get("INSTANCE_CONNECTION_NAME")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@/"
+    f"{DB_NAME}?host=/cloudsql/{INSTANCE_CONNECTION_NAME}"
 )
 
 
@@ -32,6 +38,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # DB 初期化
 # =====================
 db = SQLAlchemy(app)
+
 
 # =====================
 # ルーティング・モデル読込
